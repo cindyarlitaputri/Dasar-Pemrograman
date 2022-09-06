bbd240ac989c3607db296742662a5db3ffc725ff
cindyarlitaputri <112865161+cindyarlitaputri@users.noreply.github.com>
2022-09-06 22:45:56 +0700

    Add files via upload

---
+++ tugas 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Cindy Arlita Putri" << endl;
+    cout << "A11.2022.14154" << endl;
+    cout << "JL. Singa Tengah III / 71" << endl;
+    cout << "SMAN 2 Mranggen" << endl;
+    return 0;
+}
