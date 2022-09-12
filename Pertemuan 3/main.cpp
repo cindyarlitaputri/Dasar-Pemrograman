8da22c562ba04ad6c979ad3323fcf07626ee708c
cindyarlitaputri <112865161+cindyarlitaputri@users.noreply.github.com>
2022-09-12 17:11:24 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Cindy Arlita Putri" << endl;
+    printf ("A11.2022.14154");
+    cout << endl;
+
+    int u = 18;
+    float t = 160.5;
+    char b = 'L';
+    string ub = "XL";
+    cout << "Umur saya:" << u;
+    cout << "\nTinggi saya:" << t;
+    cout << "\nUkuran Baju saya:" << ub;
+    return 0;
+}
