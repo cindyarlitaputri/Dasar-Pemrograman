bb06a735163c8a52e16a53e2e27fabc33ca30b93
cindyarlitaputri <112865161+cindyarlitaputri@users.noreply.github.com>
2022-12-15 20:46:20 +0700

    Update main.cpp

--- pertemuan 22/main.cpp
+++ pertemuan 22/main.cpp
@@ -4,24 +4,27 @@ using namespace std;
 
 int main()
 {
-    int a = 0, b = 1, c = 0, d = 2;
+    int sum1 = 0, sum2 = 0, d = 2;
     int y[3][2][2];
     float avg;
 
+    cout << "Array Kelipatan 4 = " << endl;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 2; j++){
             for(int k = 0; k < 2; k++){
                 y[i][j][k] = d;
                 d += 2;
+                if(d % 4 == 0){
                 cout << d << " ";
-                c += d;
-                a += b;
-                avg = (float)c/a;
+                sum1 += d;
+                sum2 += 1;
+                avg = (float)sum1/sum2;
+                }
             }
         }
     }
     cout << "\n";
-    cout << "Sum Kelipatan 4 = " << c << endl;
-    cout << "Rata-rata sum kelipatan 4 = " << avg << endl;
+    cout << "Sum Kelipatan 4 = " << sum1 << endl;
+    cout << "Rata-rata Kelipatan 4 = " << avg << endl;
     return 0;
 }
